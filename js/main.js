637c1ef8647c8d7fc9e14a61ec129b7859383f9c
R1ndamanUA <123882447+R1ndamanUA@users.noreply.github.com>
2023-05-08 18:40:48 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -313,6 +313,197 @@ $(document).ready(function() {
     });
 });
 
+//======PopUP form done==================================================================================================================================================
+$('.open-popup').click(function(e) {
+	e.preventDefault();
+	$('.form-pop-up-bg').fadeIn(600);
+	$('html').addClass('no-scroll');
+});
+$('.close-popup').click(function(e) {
+	e.preventDefault();
+	$('.form-pop-up-bg').fadeOut(600);
+	$('html').removeClass('no-scroll');
+});
+$('.form-pop-up-bg').click(function (e) {
+	if (!$(e.target).closest(".form-pop-up").length) {
+		$('.form-pop-up-bg').fadeOut(600);
+		$('html').removeClass('no-scroll');
+	}
+  });
+//========================================================================================================================================================
+$(".nominations-upper-screen__button").click(function() {
+	$('html, body').animate({
+	scrollTop: $(".participation-application__container").offset().top 
+	}, 1000);
+});
+
+//=============Counter index.html===========================================================================================================================================
+(function ($) {
+	$.fn.countTo = function (options) {
+		options = options || {};
+		
+		return $(this).each(function () {
+			// set options for current element
+			var settings = $.extend({}, $.fn.countTo.defaults, {
+				from:            $(this).data('from'),
+				to:              $(this).data('to'),
+				speed:           $(this).data('speed'),
+				refreshInterval: $(this).data('refresh-interval'),
+				decimals:        $(this).data('decimals')
+			}, options);
+			
+			// how many times to update the value, and how much to increment the value on each update
+			var loops = Math.ceil(settings.speed / settings.refreshInterval),
+				increment = (settings.to - settings.from) / loops;
+			
+			// references & variables that will change with each update
+			var self = this,
+				$self = $(this),
+				loopCount = 0,
+				value = settings.from,
+				data = $self.data('countTo') || {};
+			
+			$self.data('countTo', data);
+			
+			// if an existing interval can be found, clear it first
+			if (data.interval) {
+				clearInterval(data.interval);
+			}
+			data.interval = setInterval(updateTimer, settings.refreshInterval);
+			
+			// initialize the element with the starting value
+			render(value);
+			
+			function updateTimer() {
+				value += increment;
+				loopCount++;
+				
+				render(value);
+				
+				if (typeof(settings.onUpdate) == 'function') {
+					settings.onUpdate.call(self, value);
+				}
+				
+				if (loopCount >= loops) {
+					// remove the interval
+					$self.removeData('countTo');
+					clearInterval(data.interval);
+					value = settings.to;
+					
+					if (typeof(settings.onComplete) == 'function') {
+						settings.onComplete.call(self, value);
+					}
+				}
+			}
+			
+			function render(value) {
+				var formattedValue = settings.formatter.call(self, value, settings);
+				$self.html(formattedValue);
+			}
+		});
+	};
+	
+	$.fn.countTo.defaults = {
+		from: 0,               // the number the element should start at
+		to: 0,                 // the number the element should end at
+		speed: 1000,           // how long it should take to count between the target numbers
+		refreshInterval: 100,  // how often the element should be updated
+		decimals: 0,           // the number of decimal places to show
+		formatter: formatter,  // handler for formatting the value before rendering
+		onUpdate: null,        // callback method for every time the element is updated
+		onComplete: null       // callback method for when the element finishes updating
+	};
+	
+	function formatter(value, settings) {
+		return value.toFixed(settings.decimals);
+	}
+}(jQuery));
+
+function countCallback() {
+	jQuery(function ($) {
+	  // custom formatting example
+	  $('.count-number').data('countToOptions', {
+		formatter: function (value, options) {
+		  return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ' ');
+		}
+	  });
+
+	  // start all the timers
+	  $('.timer').each(count);  
+	  
+	  function count(options) {
+		var $this = $(this);
+		options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+		$this.countTo(options);
+	  }
+	});
+};
+
+// Детектить скролл до элемента elem, вернет true или falsee.
+// Параметр elem - подставляешь элемент, которые будет в зоне видимости.
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+function runCounter() {
+	const countItems = document.querySelectorAll('.work-in-numbers');
+	if (countItems.length > 0) {
+		window.addEventListener('scroll', runCounter);
+
+		// isScrolledIntoView с параметром (countItems) это - .work-in-numbers
+		if (isScrolledIntoView(countItems)) {
+
+			// Код который отрабатывает
+			countCallback();
+
+			// убиваем ивент, чтобы не срабатывал при кажлом скроле 
+			window.removeEventListener('scroll', runCounter);
+		};
+	};
+}
+runCounter();
+
+//======Animation==================================================================================================================================================
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		$('*').off('scroll', animOnScroll);
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active');
+			} else {
+				if (!animItem.classList.contains('_anim-no-hide')) {
+					animItem.classList.remove('_active');
+				}
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+	animOnScroll();
+}
+
 //=======SWIPER=================================================================================================================================================
 new Swiper('.home-screen__swiper', {
 	navigation: {
@@ -569,4 +760,3 @@ if (!Cookies.get(COOKIE_NAME)) {
 		Cookies.set(COOKIE_NAME, true, { expires: 30 })
 	})
 }
-//========================================================================================================================================================
